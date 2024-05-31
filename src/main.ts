1ebaa3839bb1cadf47f3615ce3d70771d47bc2fa
Kenson Johnson <94240602+kensonjohnson@users.noreply.github.com>
2024-05-31 15:07:16 -0500

    Update text

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
+      Click on the Vite, TypeScript, and nginx logos to learn more
     </p>
   </div>
 `;
